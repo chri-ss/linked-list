3018d8d2dce0775389a196ad3c4142fa3fafc913
Chris Cresswell <Christorper@gmail.com>
2022-08-15 20:47:09 -0700

    Add method for appending nodes to the end of a list

--- LinkedList.js
+++ LinkedList.js
@@ -1,10 +1,25 @@
-const Node = (value = null, nextNode = null) => {
-  return { value, nextNode };
+const Node = (value = null, next = null) => {
+  return { value, next };
 };
 
 const LinkedList = (value) => {
   head = Node(value);
-  return head;
+
+  const append = (value) => {
+    temp = head;
+    while (temp.next != null) {
+      temp = temp.next;
+    }
+    temp.next = Node(value);
+  };
+  return { head, append };
 };
 
-console.log(LinkedList(3));
+const newList = LinkedList(1);
+
+console.log(newList.head);
+
+newList.append(5);
+newList.append(7);
+
+console.log(newList.head);
